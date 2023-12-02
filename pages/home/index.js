cf9d45993ef0ab89e4da9ccdcb94dd457a85b0d4
jihee <esder1310@naver.com>
2023-12-02 15:23:33 +0900

    design : Global css / home , introduce css 수정

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,17 +1,69 @@
 import React from "react";
 import Link from "next/link";
-// import TestFbase from "./testFbase";
+import styled from '@emotion/styled';
+
 
 function HomePage() {
+
   return (
     <>
-      <h1>홈페이지</h1>
-      <Link href={"/introduce"}>
-        <button>소개 페이지 이동</button>
-      </Link>
-      {/*<TestFbase />*/}
+        <Container>
+            <h1>홈페이지</h1>
+            <div
+                className={'InputDiv'}
+            >
+                <input
+                    className={'HomeInput'}
+                />
+            </div>
+            <Link href={"/introduce"}>
+                <HomeBtn
+                    type={"button"}>
+                    다음 단계로 이동
+                </HomeBtn>
+            </Link>
+            <HomeChangeLanguage>
+                <p>한국어 / 영어</p>
+            </HomeChangeLanguage>
+        </Container>
     </>
   );
 }
+const Container=styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  .InputDiv{
+    background-color: green;
+    padding: 0.5rem;
+    .HomeInput{
+      background-color: transparent;
+      border: none;
+      outline: none;
+    }
+  }
 
+`;
+const HomeBtn=styled.div`
+  padding-top: 0.7rem;
+  text-align: center;
+  align-items: center;
+  
+  width: 18.75rem;
+  height: 3.125rem;
+  flex-shrink: 0;
+
+  border-radius: 3.125rem;
+  background: #0095B5;
+  color: white;
+
+  &:hover {
+    font-weight: bold;
+    background-color: #0095B5;
+  }
+`;
+const HomeChangeLanguage=styled.div`
+  color: black;
+`;
 export default HomePage;
--- pages/introduce/index.js
+++ pages/introduce/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Link from "next/link";
-
+import Memo from "../img/memo.png";
+import Image from "next/image";
 
 function IntroducePage () {
     return (
     <>
         <h1>소개 페이지 </h1>
+        <Image src={Memo}/>
         <Link href={'/test'}>
             <button>테스트하러가기</button>
         </Link>
