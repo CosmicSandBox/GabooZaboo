906becdf8bf67ccace655b935bea57d15b7755d6
CosmicSandBox <kimjaewoo13579@gmail.com>
2023-12-05 19:12:34 +0900

    confetti 효과 추가

--- pages/result/anotherResult/[id].js
+++ pages/result/anotherResult/[id].js
@@ -2,26 +2,28 @@ import React from "react";
 import styled from "@emotion/styled";
 import FinalResultImg from "@/component/finalResultImg";
 import Link from "next/link";
-import {GrPowerReset} from "react-icons/gr";
-import {HiDownload} from "react-icons/hi";
-import {RiMenuAddLine} from "react-icons/ri";
-import {LuShare2} from "react-icons/lu";
+import { GrPowerReset } from "react-icons/gr";
+import { HiDownload } from "react-icons/hi";
+import { RiMenuAddLine } from "react-icons/ri";
+import { LuShare2 } from "react-icons/lu";
 import AnotherResultImg from "@/component/anotherResults";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
-function anotherResult () {
-    const router = useRouter()
+function anotherResult() {
+    const router = useRouter();
     const category_mbti = router.query.id;
-    console.log("category_mbti",category_mbti)
+    console.log("category_mbti", category_mbti);
 
     return (
         <>
             <Container>
                 <AnotherResultImg name={"BOO"} mbti={category_mbti} />
-                <button type={"button"} className={'Departure'}>🛫 출국하기</button>
+                <button type={"button"} className={"Departure"}>
+                    🛫 출국하기
+                </button>
                 <ResultBtns>
                     <Btns type={"button"}>
-                        <Link href={'/'}>
+                        <Link href={"/"}>
                             <GrPowerReset />
                             <p>다시하기</p>
                         </Link>
@@ -31,7 +33,7 @@ function anotherResult () {
                     {/*    <p>저장</p>*/}
                     {/*</Btns>*/}
                     <Btns type={"button"}>
-                        <Link href={'/category'}>
+                        <Link href={"/category"}>
                             <RiMenuAddLine />
                             <p>다른 결과 보러가기</p>
                         </Link>
@@ -51,68 +53,66 @@ function anotherResult () {
 
 export default anotherResult;
 
+const Container = styled.div`
+    .Departure {
+        width: 9.5625rem;
+        height: 2.3125rem;
+        flex-shrink: 0;
 
-const Container=styled.div`
+        margin-top: 1.25rem;
+        border-radius: 1.25rem;
+        background: #b5d5e9;
 
-    .Departure{
-      width: 9.5625rem;
-      height: 2.3125rem;
-      flex-shrink: 0;
+        border: none;
+        outline: none;
 
-      margin-top: 1.25rem;
-      border-radius: 1.25rem;
-      background: #B5D5E9;
-      
-      border: none;
-      outline: none;
-      
-      color: #2F5B79;
-      font-weight: 500;
-      
-      &:hover{
-        font-weight: bold;
-      }
+        color: #2f5b79;
+        font-weight: 500;
+
+        &:hover {
+            font-weight: bold;
+        }
     }
 `;
-const ResultBtns=styled.div`
-  position: absolute;
-  left:2rem;
-  bottom: 1.5rem;
+const ResultBtns = styled.div`
+    position: absolute;
+    left: 2rem;
+    bottom: 1.5rem;
 
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  
-  outline: none;
-  border: none;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+
+    outline: none;
+    border: none;
 `;
 
-const Btns=styled.button`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  
-  margin: 1rem;
+const Btns = styled.button`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
 
-  background-color: transparent;
-  color: #2F5B79;
-  text-align: center;
-  font-size: 0.625rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
+    margin: 1rem;
 
-  outline: none;
-  border: none;
-  
-  p{
-    margin-top: 0.5rem;
-  }
-  
-  &:hover{
-    font-weight: bold;
-  }
+    background-color: transparent;
+    color: #2f5b79;
+    text-align: center;
+    font-size: 0.625rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+
+    outline: none;
+    border: none;
+
+    p {
+        margin-top: 0.5rem;
+    }
+
+    &:hover {
+        font-weight: bold;
+    }
 `;
--- pages/result/index.js
+++ pages/result/index.js
@@ -1,16 +1,20 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import FirstResultImg from "@/component/firstResultImg";
 import styled from "@emotion/styled";
+import JSConfetti from "js-confetti";
 
 import GBJB from "@/img/GBJB.png";
 import Image from "next/image";
-import {getCollectionAll} from "../../firebase/fbase";
-import {useRouter} from "next/router";
+import { getCollectionAll } from "../../firebase/fbase";
+import { useRouter } from "next/router";
 
-function Result () {
-
+function Result() {
+    //HTML Canvas 요소를 생성하여 페이지에 추가
+    const jsConfetti = new JSConfetti();
     const [users, setUsers] = useState([]);
+    const router = useRouter();
+
     // export const getDocument = async (collectionName, documentId) => {
     const getUsers = async () => {
         const data = await getCollectionAll("user");
@@ -18,62 +22,76 @@ function Result () {
     };
     useEffect(() => {
         getUsers();
+
+        jsConfetti.addConfetti({
+            emojis: ["🦉", "✈️", "🌏"],
+            emojiSize: 40,
+            confettiNumber: 30,
+        });
     }, []);
+
     return (
-    <>
-        {users.map((item, index)=>(
-            <Container key={index}>
-                <Image src={GBJB} alt={"GBJB헤더"} className={"Header"}/>
-                <FirstResultImg name={item.name} mbti={item.mbti.map((value) => value.toString()).join("")}/>
-                {/*<Test />*/}
-                <Link href={`/result/secondResult/${item.mbti.map((value) => value.toString()).join("")}`}>
-                    <FirstResultBtn>
-                        <p>다음</p>
-                    </FirstResultBtn>
-                </Link>
-            </Container>
-        ))}
-
-    </>
+        <>
+            {users.map((item, index) => (
+                <Container key={index}>
+                    <Image src={GBJB} alt={"GBJB헤더"} className={"Header"} />
+                    <FirstResultImg
+                        name={item.name}
+                        mbti={item.mbti
+                            .map((value) => value.toString())
+                            .join("")}
+                    />
+                    {/*<Test />*/}
+                    <Link
+                        href={`/result/secondResult/${item.mbti
+                            .map((value) => value.toString())
+                            .join("")}`}
+                    >
+                        <FirstResultBtn>
+                            <p>다음</p>
+                        </FirstResultBtn>
+                    </Link>
+                </Container>
+            ))}
+        </>
     );
-  }
+}
 
-  export default Result;
+export default Result;
 
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
 
-const Container=styled.div`
-  display: flex;
-  flex-direction: column;
+    .Header {
+        position: absolute;
+        right: 1rem;
+        top: 1rem;
 
-  .Header{
-    position: absolute;
-    right: 1rem;
-    top: 1rem;
-
-    z-index: 1;
-  }
+        z-index: 1;
+    }
 `;
-const FirstResultBtn=styled.button`
-  width: 18.75rem;
-  height: 3.125rem;
-  flex-shrink: 0;
-  
-  margin-top: 3rem;
-  //position: absolute;
-  //bottom: 5rem;
-  left: 2.75rem;
+const FirstResultBtn = styled.button`
+    width: 18.75rem;
+    height: 3.125rem;
+    flex-shrink: 0;
 
-  border-radius: 3.125rem;
-  background: #F0F0F0;
-  border: none;
-  outline: none;
+    margin-top: 3rem;
+    //position: absolute;
+    //bottom: 5rem;
+    left: 2.75rem;
 
-  position: absolute;
-  bottom: 3rem;
+    border-radius: 3.125rem;
+    background: #f0f0f0;
+    border: none;
+    outline: none;
 
-  &:hover{
-    font-weight: bold;
-    color: white;
-    background-color: #0095B5;
-  }
+    position: absolute;
+    bottom: 3rem;
+
+    &:hover {
+        font-weight: bold;
+        color: white;
+        background-color: #0095b5;
+    }
 `;
